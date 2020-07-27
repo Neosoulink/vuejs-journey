7578ffa743e3fb0499cab4ef63a0f01e7e7df54c
Neosoulink <neodarksoulink@gmail.com>
2020-07-27 23:30:17 +0100

    Styling gallery btn

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -3,6 +3,12 @@
 		<!-- Light box part -->
 		<Lightbox />
 		<div class="mb-3 flex container">
+		<div class="py-2">
+			<legend>THÈME</legend>
+			<b-button @click="themeLightbox = '/'" variant="outline-primary" class="mr-2">DEFAULT <i class="fa fa-bell"></i></b-button>
+			<b-button @click="themeLightbox = 'code'" variant="outline-primary">CODES <i class="fa fa-code"></i></b-button>
+		</div>
+
 			<a
 				:href="require(`../imgs/img (${n}).jpg`)"
 				class="position-relative d-inline-block mb-2 mr-2"
@@ -20,17 +26,15 @@
 				v-for="n in 8"
 				:key="`../imgs/img (${n}).jpg`"
 			>
-				<img :src="require(`../imgs/code_(${n}).jpg`)" alt="lightImg" style="width: 200px;" />
+				<img :src="addImage(n)" alt="lightImg" style="width: 200px;" />
 			</a>
-
-			<div>
-				<button @click="themeLightbox = '/'">DEFAULT</button>
-				<button @click="themeLightbox = 'code'">CODES</button>
-			</div>
 		</div>
 
 		<!-- Slider part -->
 		<div class="container">
+			<div class="py-2">
+				<legend>CAROUSEL</legend>
+			</div>
 			<carousel>
 				<carousel-slide v-for="n in slides" :key="n" :index="n-1">
 					<div
@@ -40,8 +44,10 @@
 					<img :src="require(`../imgs/img (${n}).jpg`)" alt="slide" class="w-100" />
 				</carousel-slide>
 			</carousel>
-			<button @click="addSlide">Ajouter un slide</button>
-			<button @click="removeSlide">Enlever un slide</button>
+			<div class="py-3">
+				<b-button @click="addSlide" variant="outline-primary" class="mr-2">Ajouter un slidet <i class="fa fa-plus"></i></b-button>
+				<b-button @click="removeSlide" variant="outline-danger">Enlever un slide <i class="fa fa-times"></i></b-button>
+			</div>
 		</div>
 	</div>
 </template>
@@ -67,7 +73,15 @@ export default {
 			this.slides++;
 		},
 		removeSlide() {
+			if(this.slides > 1)
 			this.slides--;
+		},
+		addImage(n){ //thing to add event when img is not found...
+			try {
+				return require(`../imgs/img (${n}).jpg`)
+			} catch (error) {
+				console.log('hello',error);
+			}
 		}
 	}
 };
